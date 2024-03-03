3cf04f64f802b79cdc3755eda8d7f2d454d8c4cd
syed <syedmohi04@gmail.com>
2024-03-04 04:04:31 +0530

    removed OTP logic

--- routers/users.js
+++ routers/users.js
@@ -101,12 +101,12 @@ router.post("/user-otp", async (req, res) => {
 });
 
 router.post("/verify-otp", async (req, res) => {
-  const result = await Vendor.findOne({ mobile: req.body.mobile });
+  // const result = await Vendor.findOne({ mobile: req.body.mobile });
   const verifiedOTP = await OTP.findOne({ mobile: req.body.mobile });
-  if (!result)
-    return res
-      .status(400)
-      .json({ ok: false, message: "Vendor Doesn't exits." });
+  // if (!result)
+  //   return res
+  //     .status(400)
+  //     .json({ ok: false, message: "Vendor Doesn't exits." });
       // verifiedOTP?.otp == req.body.otp
   if (true) {
     await OTP.deleteOne({ mobile: req.body.mobile });
@@ -371,6 +371,8 @@ router.post("/vendor-action", async (req, res) => {
 router.post("/login-vendor", async (req, res) => {
   await OTP.deleteMany({ mobile: req.body.mobile });
   const NEW_OTP = Math.floor(1000 + Math.random() * 9000);
+  const newOtp = new OTP({ mobile: req.body.mobile, otp: NEW_OTP });
+  await newOtp.save();
   return res
   .status(200)
   .json({ ok: true, message: `OTP sent to ${req.body.mobile}` }); 
